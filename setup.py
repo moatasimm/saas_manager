8c28aea5ae200c13eeeab7970d7360472dafcfd6
Moatasim Al Masri <moatasim_m@yahoo.com>
2026-02-28 11:56:24 +0300

    Full standard structure for saas_manager

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="saas_manager",
+    version="0.0.1",
+    description="SaaS Manager for ERPNext",
+    author="opentech",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+)
